18e1fdae07461beae6e2dbf4a9bd805649a17475
Travis <kingery.travis@gmail.com>
2020-06-03 12:55:17 -0600

    Got rid of all non DnD Code

--- src/components/App.js
+++ src/components/App.js
@@ -10,38 +10,12 @@ import Search from './Search';
 class App extends React.Component {
 
   state = {
-    image: "",
-    display: "none",
-    dnd: "",
-    search: ""
+    dnd: ""
   };
 
   onTermSubmit = (term, type) => {
-    console.log(term + type);
-  }
-
-  componentDidMount() {
-    dnd.get('/spells', {
-      params: { name: "A" },
-    }).then(response => {
-      this.setState({ dnd: response.data.results[0].name });
-    });
-  }
-
-  getImageUrl = async () => {
-    /*axios.get('https://dog.ceo/api/breeds/image/random')
-    .then(response => {
-      this.setState({image: response.data.message, display: "block"});
-      console.log(response);
-    })*/
-    const response = await dogs.get();
-    this.setState({ image: response.data.message, display: "block" });
-  }
-
-  getDndData(searchTerm) {
-    this.setState({ search: searchTerm });
-    dnd.get('/spells', {
-      params: { name: searchTerm },
+    dnd.get(type, {
+      params: { name: term },
     }).then(response => {
       this.setState({ dnd: response.data.results[0].name });
     });
@@ -55,7 +29,6 @@ class App extends React.Component {
         <div className="container">
           <h1>Search the DnD Library</h1>
           <Search onFormSubmit={this.onTermSubmit} />
-          <input type="text" onChange={(e) => this.getDndData(e.target.value)} value={this.state.search} />
           <p id="dndInfo">{this.state.dnd}</p>
         </div>
       </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 class Search extends React.Component {
-    state = { term: '', type: 'Type Test' };
+    state = { term: '', type: '/spells' };
 
     onInputChange = (event) => {
         this.setState({ term: event.target.value })
